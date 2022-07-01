37e08231d174d48d1a1036e074f4ce01ad6d679a
Yunpyo An <anyunpyo@unist.ac.kr>
2022-07-01 16:52:14 +0900

    init

--- utils/argparser.py
+++ utils/argparser.py
@@ -19,10 +19,20 @@ def arg_parser():
     # define global model
     parser.add_argument('--model', type=str, default='CNN', 
         choices=MODELS, help='global model')
+    parser.add_argument('--update_algo', type=str, default='FedAVG', 
+        help='Federated algorithm for update global model')
+    parser.add_argument('--lr_global', type=float, default=0.001, help='global model learning rate')
+    parser.add_argument('--num_round', type=int, default=2000)
 
     # client setting
     parser.add_argument('--optimizer', type=str, default='SGD',
         choices=OPTIMIZER, help='local update optimizer')
+    parser.add_argument('--lr_local', type=float, default=0.1, help='local updating learning rate')
+    parser.add_argument('--batch_size_local', type=int, default=64)
+    parser.add_argument('--num_epoch_local', type=int, default=1)
+
+    # logger information
+    parser.add_argument('--logdir', type=str, default='logdir', help='root directory of logging')
 
     args = parser.parse_args()
     return args
---
+++ utils/logger.py
@@ -0,0 +1,7 @@
+"""
+logging by Tensorboard
+https://www.tensorflow.org/tensorboard?hl=ko
+"""
+
+def logdir():
+    pass
